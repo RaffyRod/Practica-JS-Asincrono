26d290c13b2ce2b4566ad0a8c0fee086cab4207e
Raffy Anyelo <elraffy3@gmail.com>
2020-05-04 16:07:50 -0400

    Promises

---
+++ cadena_promesas.js
@@ -0,0 +1,12 @@
+function calcular(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(resolve,400,5);
+    })
+}
+        function segundoCalculo(numero){
+            console.log(numero);
+            return new Promise((resolve,reject)=>{
+                setTimeout(resolve,200,'segunda Promise');
+            })
+        }
+            calcular().then(segundoCalculo).then(console.log);
